7e7a81359980eae7f46efbab4b9f942c866c5c6d
nagolove <dekar.own@gmail.com>
2023-07-26 16:38:26 +0300

    Signals, line parsing, reading config script.

--- backupd.c
+++ backupd.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <errno.h>
+#include <execinfo.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +9,23 @@
 #include <time.h>
 #include <unistd.h>
 
+static const char *script_name = ".backupdrc";
 static const char *card_dest = "/run/media/nagolove/4A3F-0331/";
+// время последней резервной записи
+static time_t time_last_backup; 
+// промежуток времени, через который делается резервная запись
+static time_t time_period;  
+
+static const char *get_script_fname() {
+    static char buf[512] = {};
+    char *home = getenv("HOME");
+    if (home)
+        strcat(buf, home);
+    strcat(buf, "/");
+    strcat(buf, script_name);
+    printf("get_script_fname: %s\n", buf);
+    return buf;
+}
 
 static const char *replacer_love() {
     static char buf[128] = {};
@@ -87,26 +105,6 @@ static struct {
         .fun = replacer_love,
     },
     */
-};
-
-// Действия выполняются находясь в каталоге флешки
-// $SOME_TEXT -> fun()
-static const char *actions[] = {
-    "$PUSHD",
-    "$CD2SDCARD",
-    "git clone /home/nagolove/ $DATE XXX",
-    "$POPD"
-    /*
-    "$DATE Z $DATE YYYY",
-    "III$DATE$DATE",
-    "$KISS_$DATE",
-    "$DATE_$KISS",
-    "$KISS $DATE",
-    "$DATE $KISS",
-    "$KISS",
-    "$LOVEDATE$LOVE",
-    "$LOVE$KISS",
-    // */
 };
 
 // Находит самый слева расположенный шаблон из всех для данной строки.
@@ -200,14 +198,15 @@ static const char *resolve(const char *in) {
     return buf;
 }
 
-static void do_actions() {
-    int actions_num = sizeof(actions) / sizeof(actions[0]);
-    for (int i = 0; i < actions_num; i++) {
+static void do_actions(char **lines, int lines_num) {
+    assert(lines);
+    assert(lines_num);
+    for (int i = 0; i < lines_num; i++) {
         char resolved_action[2048] = {};
-        printf("do_actions: '%s'\n", actions[i]);
-        strcat(resolved_action, resolve(actions[i]));
-        printf("do_actions: %s\n\n", resolved_action);
-        //system(resolved_action);
+        printf("do_actions: '%s'\n", lines[i]);
+        strcat(resolved_action, resolve(lines[i]));
+        int retcode = system(resolved_action);
+        printf("do_actions: system \"%s\" %d\n", resolved_action, retcode);
     }
 }
 
@@ -221,8 +220,6 @@ static bool has_lock() {
     fclose(lock);
     return true;
 }
-
-static time_t last_backup_time;
 
 static bool read_last_backup_time(time_t *last_time) {
     bool status = false;
@@ -259,9 +256,96 @@ _after_cleanup:
     return status;
 }
 
-static void do_backup() {
-    printf("target line found\n");
+struct Script {
+    char **lines;
+    int lines_num;
+};
 
+// удалить последний символ перевода строки
+// XXX: unix-only
+static void remove_caret(char *line) {
+    int line_len = strlen(line);
+    if (line_len > 0 && line[line_len - 1] == '\n') {
+        line[line_len - 1] = 0;
+    }
+}
+
+static void remove_comment(char *line) {
+    assert(line);
+    while (*line) {
+        if (*line == '#') {
+            *line = 0;
+            break;
+        }
+        line++;
+    }
+}
+
+// строка состоит только из пробелов и табуляций?
+static bool empty_string(const char *line) {
+    while (*line) {
+        if (*line != ' ' && *line != '\t')
+            return false;
+        line++;
+    }
+    return true;
+}
+
+struct Script script_load(const char *fname) {
+    struct Script sc = {};
+    FILE *script = fopen(fname, "r");
+    if (!script) {
+        fprintf(stderr, "script_load: could not load '%s'\n", fname);
+        return sc;
+    }
+    char line[1024] = {};
+    int lines_cap = 100;
+    sc.lines = calloc(lines_cap, sizeof(sc.lines[0]));
+    while (fgets(line, sizeof(line), script)) {
+        remove_caret(line);
+        remove_comment(line);
+
+        if (empty_string(line))
+            continue;
+
+        if (sc.lines_num == lines_cap) {
+            lines_cap *= 2;
+            sc.lines = realloc(sc.lines, lines_cap * sizeof(sc.lines[0]));
+            assert(sc.lines);
+        }
+
+        sc.lines[sc.lines_num] = malloc(sizeof(line));
+        assert(sc.lines[sc.lines_num]);
+        strncpy(sc.lines[sc.lines_num], line, sizeof(line));
+        sc.lines_num++;
+    }
+    fclose(script);
+    return sc;
+}
+
+void script_print(struct Script sc) {
+    if (!sc.lines)
+        return;
+    for (int i = 0; i < sc.lines_num; i++) {
+        printf("script_print: '%s'\n", sc.lines[i]);
+    }
+}
+
+void script_shutdown(struct Script *sc) {
+    if (!sc->lines)
+        return;
+    for (int j = 0; j < sc->lines_num; j++) {
+        free(sc->lines[j]);
+    }
+    free(sc->lines);
+    sc->lines = NULL;
+    memset(sc, 0, sizeof(*sc));
+}
+
+static void do_backup() {
+    printf("do_backup:\n");
+
+    /*
     int errcode;
     errcode = system("udisksctl mount -b /dev/sdb1");
     printf("mount with code %d\n", errcode);
@@ -279,18 +363,76 @@ static void do_backup() {
         accum, "%.4dy_%.2dm_%dd_%dh-%dm-%ds\n",
         tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec
     );
-    fprintf(accum, "unixtime %lu\n", last_backup_time);
+    fprintf(accum, "unixtime %lu\n", time_last_backup);
+    // */
 
+    struct Script sc = script_load(get_script_fname());
+    script_print(sc);
+    //do_actions(sc.lines, sc.lines_num);
+    script_shutdown(&sc);
 
+    /*
+    time_last_backup = now;
 
     fclose(accum);
 
 _after_cleanup:
 
-
     errcode = system("udisksctl unmount -b /dev/sdb1");
     printf("umount with code %d\n", errcode);
+    // */
 }
+
+void sig_sigint_handler(int sig) {
+    printf("sigint\n");
+    exit(EXIT_SUCCESS);
+}
+
+void sig_sigill_handler(int sig) {
+    printf("sigill\n");
+}
+
+void sig_sigabrt_handler(int sig) {
+    printf("sigabrt\n");
+}
+
+void sig_sigfpe_handler(int sig) {
+    printf("sigfpe\n");
+}
+
+void sig_sigsegv_handler(int sig) {
+    printf("sigsegv\n");
+    int num = 100;
+    void *trace[num];
+    int size = backtrace(trace, num);
+    backtrace_symbols_fd(trace, size, STDERR_FILENO);
+    exit(EXIT_FAILURE);
+}
+
+void sig_sigterm_handler(int sig) {
+    printf("sigterm\n");
+    exit(EXIT_SUCCESS);
+}
+
+void sig_sighup_handler(int sig) {
+    printf("sighup\n");
+}
+
+void sig_sigquit_handler(int sig) {
+    printf("sigquit\n");
+}
+
+void sig_sigtrap_handler(int sig) {
+    printf("sigtrap\n");
+}
+
+void sig_sigkill_handler(int sig) {
+    printf("sigkill\n");
+}
+
+void exit_handler() {
+}
+
 
 /*
     Попытаться прочитать с флешки последнее время обновления
@@ -301,10 +443,19 @@ _after_cleanup:
     то произвести резервное копирование.
 
     Записать на флешку время последнего копирования.
-
 */
-
 int main() {
+
+    signal(SIGINT, sig_sigint_handler);     /* Interactive attention signal.  */
+    signal(SIGILL, sig_sigill_handler);     /* Illegal instruction.  */
+    signal(SIGABRT, sig_sigabrt_handler);   /* Abnormal termination.  */
+    signal(SIGFPE, sig_sigfpe_handler);     /* Erroneous arithmetic operation.  */
+    signal(SIGSEGV, sig_sigsegv_handler);   /* Invalid access to storage.  */
+    signal(SIGTERM, sig_sigterm_handler);   /* Termination request.  */
+    signal(SIGHUP, sig_sighup_handler);     /* Hangup.  */
+    signal(SIGQUIT, sig_sigquit_handler);   /* Quit.  */
+    signal(SIGTRAP, sig_sigtrap_handler);   /* Trace/breakpoint trap.  */
+    signal(SIGKILL, sig_sigkill_handler);   /* Killed.  */
 
     /*
     time_t last_time = 0;
@@ -314,11 +465,13 @@ int main() {
     printf("time %s\n", ctime(&last_time));
     */
 
-    do_actions();
+    /*do_actions((char**)actions, sizeof(actions) / sizeof(actions[0]));*/
 
-    printf("exit(1);\n");
-    exit(1);
+    atexit(exit_handler);
 
+    do_backup();
+    /*
+     
     FILE *pipe = popen("udevadm monitor", "r");
 
     if (!pipe) {
@@ -333,6 +486,7 @@ int main() {
         printf("line %s\n", line);
         // ^UDEV  [5390.581842] change   /devices/pci0000:00/0000:00:14.0/usb1/1-2/1-2:1.0/host5/target5:0:0/5:0:0:0/block/sdb (block)
         if (strstr(line, "UDEV") && 
+            strstr(line, "add") &&
             strstr(line, "/devices/pci0000:00/") &&
             strstr(line, "block/sdb")) {
             do_backup();
@@ -341,4 +495,5 @@ int main() {
     } while (strlen(line));
 
     pclose(pipe);
+    */
 }
